385706a70dcbe0fb44027b51a4e93d83fdc0e2c8
Dennis Smink <dennissmink@gmail.com>
2019-08-23 11:30:00 +0200

    initial

---
+++ app/Jobs/TestJob.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class TestJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $timeout = 5;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        for($i = 0; $i < 10; $i ++){
+            echo $i . PHP_EOL;
+
+            sleep(1);
+        }
+    }
+}
